77bc6cf8ccffdf733efa44f47aee7efed359de49
wulikanhuaa <731648638@qq.com>
2018-12-18 16:31:35 +0800

    分支修复

--- app/src/main/java/com/wlkh/secondtest/MainActivity.java
+++ app/src/main/java/com/wlkh/secondtest/MainActivity.java
@@ -3,7 +3,7 @@ package com.wlkh.secondtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//1_0_2 修复分支
+//1_0_2 修复分支，令一个分支拉取合并
 public class MainActivity extends AppCompatActivity {
 
     @Override
