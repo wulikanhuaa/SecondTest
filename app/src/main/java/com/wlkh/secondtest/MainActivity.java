f17ba042f620d0daba065042a201150ae7e479d8
wulikanhuaa <731648638@qq.com>
2018-12-18 16:21:42 +0800

    提交本分支修改的内容

---
+++ app/src/main/java/com/wlkh/secondtest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wlkh.secondtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//这是一个紧急修复分支
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/wlkh/secondtest/Test1Activity.java
@@ -0,0 +1,25 @@
+package com.wlkh.secondtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Test1Activity extends AppCompatActivity {
+
+    TextView tv_branch;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test1);
+        tv_branch = findViewById(R.id.tv_branch);
+        tv_branch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(Test1Activity.this, "被点击了", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
