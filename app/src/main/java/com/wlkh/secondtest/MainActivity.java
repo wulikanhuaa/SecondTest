6e3aae018079afcf464213dd8e40270683ae08c6
wulikanhuaa <731648638@qq.com>
2018-12-18 16:45:17 +0800

    分支修复

--- app/src/main/java/com/wlkh/secondtest/MainActivity.java
+++ app/src/main/java/com/wlkh/secondtest/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 /**
  * 1_0_2 这是一个修复分支
+ * 开发分支修改了这个文件
  */
 public class MainActivity extends AppCompatActivity {
 
+    private int a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
