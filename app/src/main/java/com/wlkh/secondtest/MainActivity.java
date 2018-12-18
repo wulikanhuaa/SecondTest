5f81cdaf46a1516ee1c7ae42c1497ed08eb85ff8
wulikanhuaa <731648638@qq.com>
2018-12-18 12:05:42 +0800

    修改远程库文件

--- app/src/main/java/com/wlkh/secondtest/MainActivity.java
+++ app/src/main/java/com/wlkh/secondtest/MainActivity.java
@@ -3,6 +3,9 @@ package com.wlkh.secondtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 远程库的文件修改
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
